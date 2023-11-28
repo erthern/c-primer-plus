5ca76f6ef9918fb0d886a47c98a635fc092dfb54
zyc <whisperwhite080@gmail.com>
2023-11-28 18:22:00 +0800

    二进制字符转十进制数值.c

--- 二进制字符转十进制数值.c
+++ 二进制字符转十进制数值.c
@@ -3,17 +3,29 @@
 int change(char *a){
     int i;
     int n = strlen(a);
-    int num;
+    int nums[n];
+    int num=0;
     for(i = 0; i < n; i++){
-        if(a[i] == '1') break;
+        nums[i]=-1;
+        if(a[i]=='1') nums[i]=n-1-i;
     }
-    n-=i;
-    printf("%d",n);
-    return 0;
+    for(i = 0; i < n; i++){
+        if(nums[i]==-1) continue;
+        num+=pow1(nums[i]);
+    }
+    return num;
+}
+int pow1(int n){
+    int i;
+    int res = 1;
+    for(i = 0; i < n; i++){
+        res *= 2;
+    }
+    return res;
 }
 int main(){
     char a[100];
     scanf("%s",a);
-    change(a);
+    printf("%d",change(a));
     return 0;
 }
